507cf40a7e64d726a95ff2b5ca1dc4121a92129e
tarassov2022 <100687122+tarassov2022@users.noreply.github.com>
2022-03-18 10:45:09 +0300

    Update Main.java

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -5,9 +5,7 @@ import planes.Plane;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class Main {
-
 
     static List<Plane> planes = Arrays.asList(
             new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
@@ -23,8 +21,7 @@ public class Main {
             new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
             new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
             new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
-            new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
-    );
+            new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT) );
 
     public static void main(String[] args) {
         Airport airport = new Airport(planes);
@@ -45,6 +42,5 @@ public class Main {
         airport.printPlane(airport.sortByMaxSpeed());
         System.out.println("\nPassenger plane with max passenger capacity: ");
         System.out.println(passengerAirport.getPassengerPlaneWithMaxPassengersCapacity());
-
     }
 }
